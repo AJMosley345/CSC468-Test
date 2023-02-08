c97d3285fed380f2ae5028e04e87c7913b344ed6
Anthony Mosley <59812149+AJMosley345@users.noreply.github.com>
2023-02-08 09:19:51 -0500

    Create profile.py

---
+++ profile.py
@@ -0,0 +1,17 @@
+import geni.portal as portal
+import geni.rspec.pg as rspec
+
+# Create a Request object to start building the RSpec.
+request = portal.context.makeRequestRSpec()
+# Create a XenVM
+node = request.XenVM("node")
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
+node.routable_control_ip = "true"
+
+node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
+node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y nginx mysql-server "))
+node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status nginx'))
+node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status mysql-server'))
+
+# Print the RSpec to the enclosing page.
+portal.context.printRequestRSpec()
