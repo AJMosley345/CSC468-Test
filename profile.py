587c069260a2f8a23a85107a25b141c91ceefc97
AJ Mosley <ajmosley345@gmail.com>
2023-02-09 08:06:49 -0500

    Add another node

--- profile.py
+++ profile.py
@@ -4,14 +4,23 @@ import geni.rspec.pg as rspec
 # Create a Request object to start building the RSpec.
 request = portal.context.makeRequestRSpec()
 # Create a XenVM
-node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
-node.routable_control_ip = "true"
+node1 = request.XenVM("node")
+node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
+node1.routable_control_ip = "true"
 
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y nginx mysql-server "))
-node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status nginx'))
-node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status mysql-server'))
+node1.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
+node1.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y mysql-server "))
+node1.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status mysql-server'))
+
+# Creating another node
+node2 = request.XenVM("node")
+node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
+node2.routable_control_ip = "true"
+
+node2.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
+node2.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y nginx"))
+node2.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status nginx'))
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
+
