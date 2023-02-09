6e0f9ff2acb7f3a88d8b77a0db1d80d42523968a
AJ Mosley <ajmosley345@gmail.com>
2023-02-09 08:37:28 -0500

    Change profile.py

--- profile.py
+++ profile.py
@@ -4,7 +4,7 @@ import geni.rspec.pg as rspec
 # Create a Request object to start building the RSpec.
 request = portal.context.makeRequestRSpec()
 # Create a XenVM
-node1 = request.XenVM("node")
+node1 = request.XenVM("node1")
 node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
 node1.routable_control_ip = "true"
 
@@ -13,7 +13,7 @@ node1.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y mys
 node1.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status mysql-server'))
 
 # Creating another node
-node2 = request.XenVM("node")
+node2 = request.XenVM("node2")
 node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
 node2.routable_control_ip = "true"
 
