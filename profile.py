ddc3cb3244a36691d2af5a1402fac9f523698db0
AJ Mosley <ajmosley345@gmail.com>
2023-02-13 10:22:29 -0500

    Automated

--- profile.py
+++ profile.py
@@ -16,23 +16,32 @@ node1.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl enable m
 node1.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status mysql-server'))
 
 # Configuring mysql-server
-node1.addService(rspec.Execute(shell="/bin/sh", command='sudo mysql_secure_installation'))
-node1.addService(rspec.Execute(shell="/bin/sh", command='2'))
-node1.addService(rspec.Execute(shell="/bin/sh", command='dfA2%Vf!Z&U12mz3FlW!'))
-node1.addService(rspec.Execute(shell="/bin/sh", command='dfA2%Vf!Z&U12mz3FlW!'))
-node1.addService(rspec.Execute(shell="/bin/sh", command='y'))
-node1.addService(rspec.Execute(shell="/bin/sh", command='y'))
-node1.addService(rspec.Execute(shell="/bin/sh", command='y'))
-node1.addService(rspec.Execute(shell="/bin/sh", command='y'))
-node1.addService(rspec.Execute(shell="/bin/sh", command='y'))
+lines = [
+    "MYSQL_ROOT_PASSWORD='test@123'\n",
+    'set timeout 10\n',
+    'spawn mysql_secure_installation\n',
+    'expect "Change the password for root ?\(Press y\|Y for Yes, any other key for No\) :"\nsend "y"\n',
+    'expect "New password:"\nsend "$MYSQL_ROOT_PASSWORD"\n',
+    'expect "Re-enter new password:"\nsend "$MYSQL_ROOT_PASSWORD"\n',
+    'expect "Do you wish to continue with the password provided?\(Press y\|Y for Yes, any other key for No\) :"\nsend "y"\n',
+    'expect "Remove anonymous users?\(Press y\|Y for Yes, any other key for No\) :"\nsend "y"\n',
+    'expect "Disallow root login remotely?\(Press y\|Y for Yes, any other key for No\) :"\nsend "y"\n',
+    'expect "Remove test database and access to it?\(Press y\|Y for Yes, any other key for No\) :"\nsend "y"\n',
+    'expect "Reload privilege tables now?\(Press y\|Y for Yes, any other key for No\) :"\nsend "y"\n'
+    ]
+with open('automate_mysql', 'w') as f:
+    for line in lines:
+        f.write(line)
+node1.addService(rspec.Execute(shell="/bin/sh", command='chmod 755 automate_mysql'))
+node1.addService(rspec.Execute(shell="/bin/sh", command='./automate_mysql'))
 
 
-# Creating a default user
-node1.addService(rspec.Execute(shell="/bin/sh", command='sudo mysql -u root -p'))
-node1.addService(rspec.Execute(shell="/bin/sh", command='dfA2%Vf!Z&U12mz3FlW!'))
-node1.addService(rspec.Execute(shell="/bin/sh", command="CREATE USER 'amosley'@'localhost' IDENTIFIED BY 'OQsl*Jcn^MLntHVFn3i2';"))
-node1.addService(rspec.Execute(shell="/bin/sh", command="GRANT ALL PRIVILEGES ON *.* TO 'amosley'@'localhost' WITH GRANT OPTION;"))
-node1.addService(rspec.Execute(shell="/bin/sh", command='exit'))
+# # Creating a default user
+# node1.addService(rspec.Execute(shell="/bin/sh", command='sudo mysql -u root -p'))
+# node1.addService(rspec.Execute(shell="/bin/sh", command='dfA2%Vf!Z&U12mz3FlW!'))
+# node1.addService(rspec.Execute(shell="/bin/sh", command="CREATE USER 'amosley'@'localhost' IDENTIFIED BY 'OQsl*Jcn^MLntHVFn3i2';"))
+# node1.addService(rspec.Execute(shell="/bin/sh", command="GRANT ALL PRIVILEGES ON *.* TO 'amosley'@'localhost' WITH GRANT OPTION;"))
+# node1.addService(rspec.Execute(shell="/bin/sh", command='exit'))
 
 # Creating the nginx server
 node2 = request.XenVM("nginx-server")
