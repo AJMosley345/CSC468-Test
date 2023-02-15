f64514116a477baaf4354677bb16d22dd45dec1c
AJ Mosley <ajmosley345@gmail.com>
2023-02-15 08:23:38 -0500

    Remove reference to dockerfile

--- profile.py
+++ profile.py
@@ -6,11 +6,11 @@ request = portal.context.makeRequestRSpec()
 
 # Creating the mongodb docker container
 mongo_db = request.DockerContainer("mongo_db")
-mongo_db.docker_dockerfile = "https://github.com/AJMosley345/CSC468-Test/mongo_db/Dockerfile"
+#mongo_db.docker_dockerfile = "https://github.com/AJMosley345/CSC468-Test/mongo_db/Dockerfile"
 
 # Creating the nginx docker container
 web_server = request.DockerContainer("web_server")
-web_server.docker_dockerfile = "https://github.com/AJMosley345/CSC468-Test/nginx/Dockerfile"
+#web_server.docker_dockerfile = "https://github.com/AJMosley345/CSC468-Test/nginx/Dockerfile"
 
 # Creating a LAN to put the containers into
 lan = request.LAN("docker_lan")
