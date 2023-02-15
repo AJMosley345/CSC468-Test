12bf232f4d0775066da05e1695c48ef7b88936b9
AJ Mosley <ajmosley345@gmail.com>
2023-02-15 08:28:45 -0500

    Basic setup

--- profile.py
+++ profile.py
@@ -6,17 +6,19 @@ request = portal.context.makeRequestRSpec()
 
 # Creating the mongodb docker container
 mongo_db = request.DockerContainer("mongo_db")
+mongo_db.docker_ssh_style = "exec"
+mongo_db.docker_exec_shell = "/bin/bash"
 #mongo_db.docker_dockerfile = "https://github.com/AJMosley345/CSC468-Test/mongo_db/Dockerfile"
 
 # Creating the nginx docker container
-web_server = request.DockerContainer("web_server")
+# web_server = request.DockerContainer("web_server")
 #web_server.docker_dockerfile = "https://github.com/AJMosley345/CSC468-Test/nginx/Dockerfile"
 
 # Creating a LAN to put the containers into
-lan = request.LAN("docker_lan")
-mongodb_iface = mongo_db.addInterface("if1")
-nginx_iface = web_server.addInterface("if1")
-lan.addInterface(mongodb_iface)
-lan.addInterface(nginx_iface)
+# lan = request.LAN("docker_lan")
+# mongodb_iface = mongo_db.addInterface("if1")
+# nginx_iface = web_server.addInterface("if1")
+# lan.addInterface(mongodb_iface)
+# lan.addInterface(nginx_iface)
 
 portal.context.printRequestRSpec()
